22d9cb26b4ef69f124ce63f5e9fd28ecc63a5047
ntorrent5 <nyly05@gmail.com>
2023-05-19 11:27:03 +0200

    explicació intercanvi variables

--- intercanvi variables/main.cpp
+++ intercanvi variables/main.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int main(){
-    int x,y,aux;
+    int x,y,aux; //utilitzarem una variable auxiliar per tal de guardar el valor de x i d'aquesta manera no perdre'l
+    /*Exemple
+        x = 9;
+        y = 3;
+
+    Resultat
+        x = 3;
+        y = 9;
+    */
     cout<<"valor x: "; cin>>x;
     cout<<"valor y: "; cin>>y;
     aux = x;
