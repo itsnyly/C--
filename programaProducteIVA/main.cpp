b321f169c93bc1b9e838654d099a82d7efe9a03a
ntorrent5 <nyly05@gmail.com>
2023-05-19 10:55:44 +0200

    calcular producte amb iva

---
+++ programaProducteIVA/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>;
+using namespace std;
+
+int main(){
+    float preu, preuFinal;
+    int iva = 21;
+    cout << "Introdueix el preu del producte: ";
+    cin >> preu;
+    preuFinal = preu + (preu * iva / 100);
+    cout << "El preu final es: " << preuFinal << endl;
+    return 0;
+}
