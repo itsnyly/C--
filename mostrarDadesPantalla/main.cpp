427a660f18b4150bcd890595cb4eabb1c41006e2
ntorrent5 <nyly05@gmail.com>
2023-05-19 11:02:29 +0200

    mostrar dades per pantalla

---
+++ mostrarDadesPantalla/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>;
+using namespace std;
+
+int main(){
+    int edat;
+    string sexe;
+    float altura;
+    cout<<"Entra la teva edat: ";
+    cin>>edat;
+    cout<<"Especifica el teu sexe: ";
+    cin>>sexe;
+    cout<<"Entra la teva altura: ";
+    cin>>altura;
+    cout<<"La teva edat és: " <<edat <<endl;
+    cout<<"El teu sexe és: " <<sexe <<endl;
+    cout<<"La teva altura és: " <<altura <<endl;
+}
