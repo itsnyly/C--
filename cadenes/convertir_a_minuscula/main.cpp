7821b6a72edbb9720a40c4320584757eb0d95447
ntorrent5 <nyly05@gmail.com>
2023-05-31 12:21:08 +0200

    convertir cadena a minúscula

---
+++ cadenes/convertir_a_minuscula/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char paraulaMaj[] = "BON DIA TINGUEU";
+
+    strlwr(paraulaMaj);
+
+    cout << paraulaMaj;
+
+    return 0;
+}
