96927417dd19deaef590d3c8564b45bca8ddec05
ntorrent5 <nyly05@gmail.com>
2023-05-31 11:48:02 +0200

    copiar cadena

---
+++ cadenes/copia_cadena/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+    char paraula [] = "Programant amb en Nil";
+    char paraulaCopiada [50];
+    strcpy(paraulaCopiada,paraula);
+
+    cout<<"Cadena Original : " <<paraula <<endl;
+    cout<<"Cadena Copiada : " <<paraulaCopiada;
+
+    return 0;
+}
