9ec3afb375f8ad8ad3a97978371fb8a851fff9ee
ntorrent5 <nyly05@gmail.com>
2023-05-31 12:18:02 +0200

    convertir cadena a majúscules

---
+++ cadenes/convetir_a_majuscula/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char paraulaMin[] = "hola, com esteu?";
+
+    strupr(paraulaMin);
+
+    cout << paraulaMin;
+
+    return 0;
+}
