aca2ad0d3f851e584a451356817afca491c051cb
ntorrent5 <nyly05@gmail.com>
2023-05-31 12:15:49 +0200

    invertir cadenes

---
+++ cadenes/invertir_cadena/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char cadenaOriginal[] = "hola, bon dia!";
+    strrev(cadenaOriginal);
+    cout << cadenaOriginal;
+
+    return 0;
+}
