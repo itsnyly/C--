8d3a604ae49cb538f6da2d3e97da3308f69d6152
ntorrent5 <nyly05@gmail.com>
2023-06-01 18:45:07 +0200

    recerca binaria

---
+++ recerques/binaria/main.cpp
@@ -0,0 +1,49 @@
+/**
+ *!Per utilitzar la recerca binaria, l'array ha d'estar ordenat
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numeros[] = {1, 2, 3, 4, 5};
+    int inici, final, meitat, valor;
+    bool trobat = false;
+
+    valor = 4;
+    inici = 0;
+    final = 5;
+
+    while (inici <= final)
+    {
+        meitat = (inici + final) / 2;
+
+        if (numeros[meitat] == valor)
+        {
+            trobat = true;
+            break;
+        }
+        else if (numeros[meitat] > valor)
+        {
+            final = meitat;
+            meitat = (inici + final) / 2;
+        }
+        else
+        {
+            inici = meitat;
+            meitat = (inici + final) / 2;
+        }
+    }
+
+    if (trobat)
+    {
+        cout << "El valor s'ha trobat a la posició " << meitat;
+    }
+    else
+    {
+        cout << "El valor no existeix a l'array";
+    }
+    return 0;
+}
