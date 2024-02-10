92565f6c164ae3deb256c8cf33dac69c2e217bcc
ntorrent5 <nyly05@gmail.com>
2024-02-10 16:12:46 +0100

    funcional, falta mirar perquè algun es queda repetit

--- Acmes/Acme 0/5/main.cpp
+++ Acmes/Acme 0/5/main.cpp
@@ -37,61 +37,89 @@ void mostrar_missatge_error(string missatge){
     cout<<"<< ERROR: "<<missatge<<" >>" <<endl;
 };
 
-void mostrar_titol(string titol){
-    cout << titol;
-}
-
-void inserir_llibre(Vector_inventari_n& inventari, Llibre nou_llibre){
+void inserir_llibre(Vector_inventari_n& inventari, Llibre nou_llibre, int num_inventari){
     int i= inventari.n-1;
     while(i>=0 and inventari.vec[i].llibre.titol>nou_llibre.titol){
         inventari.vec[i+1]=inventari.vec[i];
         i--;
     }
-    inventari.vec[i+1].llibre=nou_llibre;
+    if(inventari.vec[i].llibre.titol == nou_llibre.titol) inventari.vec[i].stock_2 = nou_llibre.stock;
+    else{
+        inventari.vec[i+1].llibre=nou_llibre;
+        if(num_inventari == 1) inventari.vec[i+1].stock_1 = nou_llibre.stock;
+        else inventari.vec[i+1].stock_2 = nou_llibre.stock;
+    }
+    
     inventari.n++;
 };
 
-void llegir_info_fitxer_inventari(Vector_inventari_n& inventari, ifstream& f_in){
+void llegir_info_fitxer_inventari(Vector_inventari_n& inventari, ifstream& f_in, int num_inventari){
     Llibre nou_llibre=llegir_llibre(f_in);
         while(not f_in.eof()){
-            inserir_llibre(inventari, nou_llibre);
+            inserir_llibre(inventari, nou_llibre, num_inventari);
             nou_llibre=llegir_llibre(f_in);
         }
 };
-void omplir_de_fitxer_inventari(Vector_inventari_n& inventari){
-    inventari.n=0;
-    string nom;
-    cout<<"ENTRA EL NOM DE FITXER DE L'INVENTARI DE LA LLIBRERIA 1: ";
-    cin>>nom;
-    ifstream f_in(nom.c_str());
+
+void comprovar_fitxers(Vector_inventari_n& inventari,string fitxer, int num_inventari){
+    ifstream f_in(fitxer.c_str());
     if(f_in.is_open()){
-        llegir_info_fitxer_inventari(inventari,f_in);
+        llegir_info_fitxer_inventari(inventari,f_in,num_inventari);
     }
     else{
         mostrar_missatge_error("EL FITXER NO S'HA POGUT OBRIR");
         cout<<"INTRODUEIX UN ALTRE NOM DE FITXER: ";
-        cin>>nom;
-        ifstream f_in(nom.c_str());
-        llegir_info_fitxer_inventari(inventari,f_in);
+        cin>>fitxer;
+        ifstream f_in(fitxer.c_str());
+        llegir_info_fitxer_inventari(inventari,f_in, num_inventari);
     }
+};
 
+void omplir_de_fitxer_inventari(Vector_inventari_n& inventari){
+    inventari.n=0;
+    string fitxer1;
+    cout<<"ENTRA EL NOM DE FITXER DE L'INVENTARI DE LA LLIBRERIA 1: " << endl;
+    cin>>fitxer1;
+    comprovar_fitxers(inventari, fitxer1, 1);
+    string fitxer2;
+    cout<<"ENTRA EL NOM DE FITXER DE L'INVENTARI DE LA LLIBRERIA 2: " << endl;
+    cin>>fitxer2;
+    comprovar_fitxers(inventari, fitxer2, 2);
+};
+
+void ordenar_inventari_isbn(Vector_inventari_n& inventari_original){
+    for (int i = 0; i < inventari_original.n; i++)
+    {
+        for (int j = 0; j < inventari_original.n; j++)
+        {
+            if(inventari_original.vec[j].llibre.isbn > inventari_original.vec[i].llibre.isbn){
+                Inventari aux = inventari_original.vec[i];
+                inventari_original.vec[i] = inventari_original.vec[j];
+                inventari_original.vec[j] = aux;
+            }
+        }
+    }
 };
 
 void mostrar_inventari(Inventari inventari){
-    cout << left << setfill(' ');
-    cout << inventari.llibre.titol << " " << inventari.llibre.isbn << " " << inventari.llibre.autor <<  " " << inventari.llibre.editorial << " " << inventari.llibre.stock << endl; 
+    cout << inventari.stock_1 << " - " << inventari.stock_2 << " " << inventari.llibre.titol << ", " << inventari.llibre.autor << " Ed. " << inventari.llibre.editorial <<  " - " << inventari.llibre.isbn << endl; 
 };
 
 void mostrar_informacio_inventari(const Vector_inventari_n inventari){
+    cout << "Estocs Titol, Autor Editorial - ISBN" << endl;
     for (int i = 0; i < inventari.n; i++)
     {
         mostrar_inventari(inventari.vec[i]);
     }
+    cout << endl;
 };
 int main(){
     Vector_inventari_n inventari;
     omplir_de_fitxer_inventari(inventari);
+    cout << "INVENTARI PER ORDRE ALFABETIC" << endl;
     mostrar_informacio_inventari(inventari);
-
+    cout << "INVENTARI PER ORDRE D'ISBN" << endl;
+    ordenar_inventari_isbn(inventari);
+    mostrar_informacio_inventari(inventari);
     return 0;
 }
