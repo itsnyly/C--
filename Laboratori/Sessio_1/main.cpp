47102f5e2a374af9780563c66a3444fd9bae1c17
ntorrent5 <nyly05@gmail.com>
2024-02-24 19:03:36 +0100

    pre i post de totes les funcions

--- Laboratori/Sessio_1/main.cpp
+++ Laboratori/Sessio_1/main.cpp
@@ -28,6 +28,8 @@ struct Vector_categories_n {
 };
 
 void guardar_data_referencia(Data& data_referencia){
+    //Pre: data
+    //Post: guarda la data introduida per l'usuari
     cout << "Entra una data (any mes dia):" << endl;
     cin >> data_referencia.any >> data_referencia.mes >> data_referencia.dia;
 }
@@ -59,27 +61,21 @@ void inserir_categoria(Vector_categories_n& categories, string nova_categoria, i
 
 void mostrar_producte(Producte producte) {
 //Pre: cert
-//Post: mostra la informació de la jugadora a per pantalla
+//Post: mostra la informació del producte per pantalla
     cout<<"c:"<<producte.codi<<" - "<<producte.nom<<" ("<<producte.marca<<") - n:"<<producte.nombre_unitats<<", cad:"<<producte.data_caducitat.dia<<"-"<<producte.data_caducitat.mes<<"-"<<producte.data_caducitat.any<<" ["<<producte.categoria<<"]"<<endl;
-}
-
-void mostrar_categoria(Categoria categoria) {
-//Pre: cert
-//Post: mostra informació del país a per pantalla
-    cout<<categoria.nom<< " - " <<categoria.unitats_recollides<<endl;
 }
 
 void mostrar_productes(const Vector_productes_n& productes) {
 //Pre: cert
-//Post: ha mostrat la informació guardada a jugadores.vec[0..jugadores.n-1] per pantalla
+//Post: ha mostrat la informació guardada a productes.vector[0..productes.mida_vector-1] per pantalla
     cout<<"Llistat de productes de caducitat curta:"<<endl;
     for (int i=0; i<productes.mida_vector; i++)
         mostrar_producte(productes.vector[i]);
 }
 
 void cerca_dicotomica_producte(const Vector_productes_n& productes, string codi, bool& trobat, int& pos) {
-//Pre: jugadores.vec[0..jugadores.n-1] ordenat per codi de jugadora i 0<=N<=MAX_J i 0<=paisos.n<=MAX_P
-//Post: trobat és true si a jugadores.vec[0..jugadores.n-1] hi ha una jugadora amb el codi donat i pos és la posició on es troba; si no hi ha cap jugadora amb aquest codi, trobat és false i pos és la posició on hauria de ser per mantenir l'ordre de la taula
+//Pre: productes.vector[0..productes.mida_vector-1] ordenat per codi de producte i 0<=N<=MAX_PRODUCTES i 0<=categories.mida_vector<=MAX_CATEGORIES
+//Post: trobat és true si a productes.vector[0..productes.mida_vector-1] hi ha un producte amb el codi donat i pos és la posició on es troba; si no hi ha cap producte amb aquest codi, trobat és false i pos és la posició on hauria de ser per mantenir l'ordre de la taula
     int esq=0, dret=productes.mida_vector-1, mig=0;
     trobat=false;
     while (not trobat and esq<=dret) {
@@ -93,8 +89,8 @@ void cerca_dicotomica_producte(const Vector_productes_n& productes, string codi,
 }
 
 void inserir_producte(Vector_productes_n& productes, Producte nou_producte) {
-//Pre: jugadores.n=N<MAX_J i jugadores.vec[0..N-1] ordenat per codi de jugadora
-//Post: jugadores.n=N+1 i nova s'ha inserit a jugadores.vec[0..N-1] mantenint l'ordre de les jugadores
+//Pre: productes.mida_vector=N<MAX_PRODUCTES i productes.vector[0..N-1] ordenat per codi de producte
+//Post: productes.mida_vector=N+1 i nou_producte s'ha inserit a productes.vector[0..N-1] mantenint l'ordre dels productes
     int i=productes.mida_vector-1;
     while (i>=0 and productes.vector[i].codi>nou_producte.codi){
         productes.vector[i+1]=productes.vector[i];
@@ -105,6 +101,8 @@ void inserir_producte(Vector_productes_n& productes, Producte nou_producte) {
 }
 
 void eliminar_producte(Vector_productes_n& productes,int pos){
+    //Pre: productes.mida_vector=N<MAX_PRODUCTES i productes.vector[0..N-1] ordenat per codi de producte
+    //Post: productes.mida_vector=N-1 i productes.vector[pos] s'ha eliminat a productes.vector[0..N-1] mantenint l'ordre dels productes
     for (int i=pos;i<productes.mida_vector-1; i++){
         productes.vector[i]=productes.vector[i+1];
     }
@@ -112,8 +110,8 @@ void eliminar_producte(Vector_productes_n& productes,int pos){
 }
 
 void alta_producte_categoria(Vector_categories_n& categories, string nova_categoria, int nombre_unitats_producte) {
-//Pre: paisos.n=N i 0<=N<=MAX_P i paisos[0..N-1] ordenat per nom de país
-//Post: si nou_pais no apareixia a paisos.vec[0..N-1], el nou_pais s'hi ha afegit respectant l'ordre alfabètic i paisos.n=N+1; en cas contrari, s'ha incrementat una jugadora al país corresponent
+//Pre: categories.mida_vector=N i 0<=N<=MAX_CATEGORIES i categories[0..N-1] ordenat per categoria
+//Post: si nova_categoria no apareixia a categories.vector[0..N-1], la nova_categoria s'hi ha afegit respectant l'ordre alfabètic i categoreis.mida_vector=N+1; en cas contrari, es suma la quantitat de productes de nova_categoria a la categoria existent
     bool trobat;
     int pos;
     cerca_dicotomica_categoria(categories,nova_categoria,trobat,pos);
@@ -122,10 +120,14 @@ void alta_producte_categoria(Vector_categories_n& categories, string nova_catego
 }
 
 void actualitzar_unitats_producte(Producte& producte, const Producte producte_repetit){
+    //Pre: producte original i producte repetit
+    //Post: es suma el nombre d'unitats del producte repetit al producte original
     producte.nombre_unitats+=producte_repetit.nombre_unitats;
 }
 
 bool es_caducitat_curta(const Producte producte, const Data data_referencia){
+    //Pre: producte i una data de referencia
+    //Post: retorna cert si la data del producte és inferior a la data de referencia, altrament retorna fals
     if(producte.data_caducitat.any < data_referencia.any) return true;
     else if(producte.data_caducitat.any == data_referencia.any && producte.data_caducitat.mes < data_referencia.mes) return true;
     else if (producte.data_caducitat.any == data_referencia.any && producte.data_caducitat.mes == data_referencia.mes && producte.data_caducitat.dia < data_referencia.dia) return true;
@@ -133,18 +135,22 @@ bool es_caducitat_curta(const Producte producte, const Data data_referencia){
 }
 
 bool es_data_diferent(const Producte producte_trobat, const Producte nou_producte ){
+    //Pre: producte que es troba dins productes.vector[0...productes.mida_vector] i el nou producte que es vol inserir
+    //Post: retorna fals si les dates dels dos productes són iguals, altrament retorna cert
     if(producte_trobat.data_caducitat.any == nou_producte.data_caducitat.any && producte_trobat.data_caducitat.mes == nou_producte.data_caducitat.mes && producte_trobat.data_caducitat.dia == nou_producte.data_caducitat.dia) return false;
     return true;
 }
 
 void buscar_producte_repetit(const Vector_productes_n productes, const Producte nou_producte, bool& trobat, int& pos){
+    //Pre: vector de productes ordenat alfabeticament, el producte a buscar
+    //Post: Retorna cert si es troba el producte dins el vector i les seves dates de caducitat són diferents, altrament retorna fals
     cerca_dicotomica_producte(productes, nou_producte.codi, trobat, pos);
     if(trobat && es_data_diferent(productes.vector[pos], nou_producte)) trobat = false;
 }
 
 Producte llegir_producte(ifstream &f_in){
 //Pre: f_in obert apunt de llegir
-//Post: si després de la 1a lectura no s’activa f_in.eof, es llegix i retorna una jugadora de fin
+//Post: si després de la 1a lectura no s’activa f_in.eof, es llegix i retorna un producte de fin
   Producte producte;
   f_in>> producte.codi;
   if(not f_in.eof())
@@ -153,6 +159,8 @@ Producte llegir_producte(ifstream &f_in){
 }
 
 void guardar_aliments_no_repetits(Vector_productes_n& productes_caducitat_curta, Vector_productes_n& productes_caducitat_llarga, Producte nou_producte, Data data_caducitat_referencia){
+    //Pre: llistat dels productes de caducitat curta, llistat dels productes de caducitat llarga, el producte a guardar i la data de referencia
+    //Post: insereix el nou producte en un llistat o l'altre depenent de la seva data de caducitat, si el producte no està repetit 
     bool trobat = false;
     int pos;
     if(es_caducitat_curta(nou_producte, data_caducitat_referencia))
@@ -171,7 +179,7 @@ void guardar_aliments_no_repetits(Vector_productes_n& productes_caducitat_curta,
 
 void omplir_de_fitxer(Vector_productes_n& productes_caducitat_curta,Vector_productes_n& productes_caducitat_llarga, Vector_categories_n& categories, Data data_caducitat_referencia, bool& fitxer_trobat) {
 //Pre: cert
-//Post: s'han omplert jugadores i paisos amb les dades del fitxer indicat per teclat i jugadores.vec[0..jugadores.n-1] està ordenat per codi de jugadora i paisos.vec[0..paisos.n-1] està ordenat per nom de país
+//Post: s'han omplert productes i categories amb les dades del fitxer indicat per teclat i productes.vector[0..productes.mida_vector-1] està ordenat per codi de producte i categories.vector[0..categories.mida_vector-1] està ordenat per categoria
     productes_caducitat_curta.mida_vector=0;
     productes_caducitat_llarga.mida_vector=0;
     categories.mida_vector=0;
@@ -192,32 +200,44 @@ void omplir_de_fitxer(Vector_productes_n& productes_caducitat_curta,Vector_produ
 }
 
 void intercanvi_posicions_aliments(Producte& producte_1, Producte& producte_2){
+    //Pre: producte_1 i producte_2
+    //Post: guarda el valor de producte_1 a producte_2 i el de producte_2 a producte_1
     Producte aux = producte_1;
     producte_1 = producte_2;
     producte_2 = aux;
 }
 
 void ordenar_productes_per_nom(Vector_productes_n& productes){
+    //Pre: llistat de productes ordenats per codi
+    //Post: ordena el llistat alfabeticament per nom; si aquest és el mateix s'ordena per marca; i en cas que aquest també sigui el mateix s'ordenarà per data
     for (int i = 0; i < productes.mida_vector; i++)
     {
         for (int j = 1; j < productes.mida_vector; j++)
         {
-            if(productes.vector[i].nom < productes.vector[j].nom) intercanvi_posicions_aliments(productes.vector[i], productes.vector[j]);
+            if(productes.vector[i].nom > productes.vector[j].nom) intercanvi_posicions_aliments(productes.vector[i], productes.vector[j]);
+            else if (productes.vector[i].nom == productes.vector[j].nom && productes.vector[i].marca > productes.vector[j].marca) intercanvi_posicions_aliments(productes.vector[i], productes.vector[j]);
+            else if (productes.vector[i].nom == productes.vector[j].nom && productes.vector[i].marca == productes.vector[j].marca && !es_caducitat_curta(productes.vector[i],productes.vector[j].data_caducitat)) intercanvi_posicions_aliments(productes.vector[i], productes.vector[j]);
         }
     }
 }
 
 void ordenar_productes_per_data(Vector_productes_n& productes){
+    //Pre: llistat de productes ordenats per codi
+    //Post: ordena el llistat alfabeticament per data; si aquest és el mateix s'ordena per nom; i en cas que aquest també sigui el mateix s'ordenarà per marca
     for (int i = 0; i < productes.mida_vector; i++)
     {
         for (int j = 1; j < productes.mida_vector; j++)
         {
-            if(es_caducitat_curta(productes.vector[j], productes.vector[i].data_caducitat)) intercanvi_posicions_aliments(productes.vector[i], productes.vector[j]);
+            if(!es_caducitat_curta(productes.vector[i], productes.vector[j].data_caducitat)) intercanvi_posicions_aliments(productes.vector[i], productes.vector[j]);
+            else if (!es_data_diferent(productes.vector[i],productes.vector[j]) && productes.vector[i].nom > productes.vector[j].nom) intercanvi_posicions_aliments(productes.vector[i], productes.vector[j]);
+            else if (!es_data_diferent(productes.vector[i],productes.vector[j]) && productes.vector[i].nom == productes.vector[j].nom && productes.vector[i].marca > productes.vector[j].marca) intercanvi_posicions_aliments(productes.vector[i], productes.vector[j]);
         }
     }
 }
 
 void comprovar_comanda(Vector_categories_n categories, string nom_categoria, int quantitat_comanda){
+    //Pre: llistat de categories i comanda a realitzar
+    //Post: si la categoria de la comanda es troba dins del llistat i la quanitat de la comanda es < que el nombre d'unitats d'aquella categoria, es pot satisfer la comanda, altrament no es podrà satisfer
     bool trobat = false;
     int pos;
     cerca_dicotomica_categoria(categories,nom_categoria,trobat,pos);
@@ -229,6 +249,8 @@ void comprovar_comanda(Vector_categories_n categories, string nom_categoria, int
 }
 
 void actualitzar_llistes_aliments(Vector_productes_n& productes_caducitat_curta, Vector_productes_n& productes_caducitat_llarga, const Data nova_data_referencia){
+    //Pre: llistat de productes de caducitat llarga, llistat de productes de caducitat curta i la nova data de caducitat
+    //Post: els productes de caducitat llarga que tinguin una data < que nova_data_referencia es guardaran al llistat de productes de caducitat curta i s'elimina de l'anterior
     for (int i = 0; i < productes_caducitat_llarga.mida_vector; i++)
     {
         if(es_caducitat_curta(productes_caducitat_llarga.vector[i],nova_data_referencia)) 
@@ -303,6 +325,6 @@ int main (){
             opcio=llegir_opcio();
         }
     }
-    
+    else cout << "Fitxer no trobat";
     return 0;
 }
